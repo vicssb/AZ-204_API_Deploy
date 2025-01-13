1fac5680bd6d18d2697b6f25440d6d83554a02f1
Victor <vicssb@gmail.com>
2025-01-13 16:41:43 -0300

    transfer from Azure.

--- APITempoAZ204/WeatherForecast.cs
+++ APITempoAZ204/WeatherForecast.cs
@@ -8,6 +8,8 @@ namespace APITempoAZ204
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        public int TemperatureK => 273 + TemperatureC;
+
         public string? Summary { get; set; }
     }
 }
